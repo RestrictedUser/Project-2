a3eee25559b6fb3a097d91700aed414e6e57769d
Chong Thao <tsoogthoj@hotmail.com>
2018-12-15 09:40:10 -0800

    master

--- server.js
+++ server.js
@@ -1,47 +1,31 @@
-require("dotenv").config();
-var express = require("express");
-var exphbs = require("express-handlebars");
+let express = require("express");
 
-var db = require("./models");
+// Sets up the Express App
+// =============================================================
+let app = express();
+let PORT = process.env.PORT || 8080;
 
-var app = express();
-var PORT = process.env.PORT || 3000;
+// Requiring our models for syncing
+let db = require("./models");
 
-// Middleware
-app.use(express.urlencoded({ extended: false }));
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// Static directory
 app.use(express.static("public"));
 
-// Handlebars
-app.engine(
-  "handlebars",
-  exphbs({
-    defaultLayout: "main"
-  })
-);
-app.set("view engine", "handlebars");
-
 // Routes
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
+// =============================================================
+require("./routes/html-routes.js")(app);
+require("./routes/user-api-routes.js")(app);
+require("./routes/goal-api-routes.js")(app);
 
-var syncOptions = { force: false };
-
-// If running a test, set syncOptions.force to true
-// clearing the `testdb`
-if (process.env.NODE_ENV === "test") {
-  syncOptions.force = true;
-}
-
-// Starting the server, syncing our models ------------------------------------/
-db.sequelize.sync(syncOptions).then(function() {
+// Syncing our sequelize models and then starting our Express app
+// =============================================================
+// db.sequelize.sync({ force: true }).then(function() {
+db.sequelize.sync().then(function() { 
   app.listen(PORT, function() {
-    console.log(
-      "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
-      PORT,
-      PORT
-    );
+    console.log("App listening on localhost:" + PORT);
   });
 });
-
-module.exports = app;
