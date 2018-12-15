129cd4b96f0890553b700ded6521ff0136a51729
Chong Thao <tsoogthoj@hotmail.com>
2018-12-15 12:45:31 -0800

    restart

--- models/habit.js
+++
@@ -1,9 +0,0 @@
-module.exports = function(sequelize, DataTypes) {
-  var Habit = sequelize.define("habit", {
-    nameFirst: DataTypes.STRING,
-    nameLast: DataTypes.STRING,
-    habit: DataTypes.STRING,
-    completion: DataTypes.BOOLEAN
-  });
-  return Habit;
-};
